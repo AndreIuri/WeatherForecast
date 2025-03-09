d500710e2ab0e1eea472ced348da8244feb75ac3
André Luís <andre.luis@onesource.pt>
2025-03-09 18:09:46 +0000

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-// App.tsx
-import './App.css';
-import { Route, Routes } from 'react-router-dom';  // Make sure to use Routes instead of just Route
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
 import { Home } from './pages/home/Home';
 import { Weather } from './pages/weather/Weather';
 
 function App() {
   return (
-    <>
-      {/* No need to wrap this with Router */}
+    <Router basename="/WeatherForecast">  {/* ✅ Only one Router */}
       <Routes>  
         <Route path="/" element={<Home />} />
         <Route path="/weather" element={<Weather />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
