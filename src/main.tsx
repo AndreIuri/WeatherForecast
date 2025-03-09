5fb3474c51958e87534fc9e36b812d799254a27d
André Luís <andre.luis@onesource.pt>
2025-03-09 14:42:47 +0000

    change main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  BrowserRouter,
-} from "react-router-dom";
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';  // Import Router
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+// Wrap the Router with a basename
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router basename="/WeatherForecast">  {/* Set the basename here */}
+    <App />
+  </Router>
+);
