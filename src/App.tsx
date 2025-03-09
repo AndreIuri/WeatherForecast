38281a95f21f1b1b70e177b9079839b00e942739
André Luís <andre.luis@onesource.pt>
2025-03-09 22:10:43 +0000

    remove variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
 import {Home} from './pages/home/Home'
 import {Weather} from './pages/weather/Weather'
-import Temperature from './pages/graphs/GraphTemperature'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -25,10 +18,6 @@ function App() {
               <Route
                   path="/weather"
                   element={<Weather />}
-              />
-              <Route
-                  path="/temperature"
-                  element={<Temperature />}
               />
           </Routes>
     </>
