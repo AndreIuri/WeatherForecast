b7c9e10dc9218737946dee02a0b44cce29361f4a
André Luís <andre.luis@onesource.pt>
2025-03-09 17:55:14 +0000

    version update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
+// App.tsx
 import './App.css';
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';  // Use HashRouter and Link
+import { Route, Routes } from 'react-router-dom';  // Make sure to use Routes instead of just Route
 import { Home } from './pages/home/Home';
 import { Weather } from './pages/weather/Weather';
 
 function App() {
   return (
-    <Router>  {/* Wrap the app with HashRouter */}
-      <div className="d-flex flex-column h-100">
-        <header>
-          {/* Updated navigation using Link component */}
-          <nav>
-            <ul>
-              <li>
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li>
-                <Link className="nav-link" to="/weather">Weather</Link>
-              </li>
-              {/* Add more navigation links here */}
-            </ul>
-          </nav>
-        </header>
-        <main className="flex-shrink-0">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/weather" element={<Weather />} />
-            {/* Add more routes here */}
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <>
+      {/* No need to wrap this with Router */}
+      <Routes>  
+        <Route path="/" element={<Home />} />
+        <Route path="/weather" element={<Weather />} />
+      </Routes>
+    </>
   );
 }
 
